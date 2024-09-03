b57d176ee0c6aa591e69a823664f44927f43813f
KCFindstr <792491320@qq.com>
2024-09-03 01:33:52 -0700

    Add github actions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # @rurino/react-utils
 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![NPM package](https://img.shields.io/npm/v/%40rurino%2Freact-utils)](https://www.npmjs.com/package/@rurino/react-utils)
+[![Build](https://github.com/rurimegu/react-utils/actions/workflows/build.yaml/badge.svg)](https://github.com/rurimegu/react-utils/actions/workflows/build.yaml)
+
 React components and library functions for the rurino project.
--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,26 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier/recommended';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      prettier,
+    ],
+    files: ['**/*.{js,ts,tsx}'],
+    ignores: [
+      '.eslintrc.cjs',
+      'build.js',
+      '*.config.ts',
+      '*.config.js',
+      '*.config.mjs',
+    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,6 +35,38 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      eqeqeq: 'warn',
+      'max-params': 'off',
+      'no-duplicate-imports': 'warn',
+      'no-return-await': 'warn',
+      'no-implicit-coercion': 'warn',
+      'linebreak-style': ['error', 'unix'],
+      'no-useless-constructor': 'off',
+      'guard-for-in': 'off',
+      'no-void': 'off',
+      'react/require-default-props': 'off',
+      'no-shadow': 'off',
+      'no-redeclare': 'off',
+      '@typescript-eslint/no-shadow': 'warn',
+      '@typescript-eslint/no-redeclare': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        2,
+        { args: 'all', argsIgnorePattern: '^_' },
+      ],
     },
   },
-)
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
